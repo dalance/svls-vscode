db334e814c7fb1e4be92484000b154dcb2020b63
dalance <dalance@gmail.com>
2019-11-15 15:21:56 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# svls-vscode
+
+SystemVerilog language server client for Visual Studio Code
+
+## Features
+
+This extension is a client of [svls](https://github.com/dalance/svls): SystemVerilog language server.
+
+## Requirements
+
+[svls](https://github.com/dalance/svls) must be installed to any directory in PATH.
---
+++ src/extension.ts
@@ -0,0 +1,44 @@
+import * as path from 'path';
+import { workspace, ExtensionContext } from 'vscode';
+
+import {
+	LanguageClient,
+	LanguageClientOptions,
+	ServerOptions,
+	TransportKind
+} from 'vscode-languageclient';
+
+let client: LanguageClient;
+
+export function activate(context: ExtensionContext) {
+	// If the extension is launched in debug mode then the debug server options are used
+	// Otherwise the run options are used
+	let serverOptions: ServerOptions = {
+		run: {command: "svls"},
+		debug: {command: "svls", args: ["--debug"]},
+	};
+
+	// Options to control the language client
+	let clientOptions: LanguageClientOptions = {
+		// Register the server for plain text documents
+		documentSelector: [{ scheme: 'file', language: 'systemverilog' }],
+	};
+
+	// Create the language client and start the client.
+	client = new LanguageClient(
+		'svls',
+		'SystemVerilog language server',
+		serverOptions,
+		clientOptions
+	);
+
+	// Start the client. This will also launch the server
+	client.start();
+}
+
+export function deactivate(): Thenable<void> | undefined {
+	if (!client) {
+		return undefined;
+	}
+	return client.stop();
+}
